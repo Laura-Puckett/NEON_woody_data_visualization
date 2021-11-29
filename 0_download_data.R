81032ee58b9697c9b8f96731d620f07aab9aa364
Laura-Puckett <lkp58@nau.edu>
2021-11-29 14:24:16 -0700

    added rmarkdown version of code

--- 0_download_data.R
+++ 0_download_data.R
@@ -12,10 +12,10 @@
 
 library(neonUtilities); 
 
-# instructions for getting a NEON API token can be found here: https://www.neonscience.org/resources/learning-hub/tutorials/neon-api-tokens-tutorial
-
 # load my neon token as an object called NEON_TOKEN
 source('./neon_token.R') 
+
+# instructions for getting a NEON API token can be found here: https://www.neonscience.org/resources/learning-hub/tutorials/neon-api-tokens-tutorial
 
 veglist = loadByProduct(dpID="DP1.10098.001",
                         site = "all",
